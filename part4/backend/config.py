c059142788c8ec257b886bd35250ceca9ac4aa1c
federico-paganini <perfedefer@gmail.com>
2025-07-25 20:28:52 -0300

    feat(config): switch to PostgreSQL engine, add dotenv support, and create postgres user for DB management
    - Updated SQLALCHEMY_DATABASE_URI to use PostgreSQL.
    - Integrated python-dotenv for environment variable management.
    - Added instructions and config for creating a dedicated postgres user.

--- part4/backend/config.py
+++ part4/backend/config.py
@@ -1,15 +1,27 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
     DEBUG = False
 
+    DB_USER = os.getenv("DB_USER", "usuario_por_defecto")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "contraseña")
+    DB_NAME = os.getenv("DB_NAME", "basededatos")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_PORT = os.getenv("DB_PORT", "5432")
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///development.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 config = {"development": DevelopmentConfig, "default": DevelopmentConfig}
