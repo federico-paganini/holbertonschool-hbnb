6aea03ef863bccc580603a3d4e650ea185bb97ab
federico-paganini <perfedefer@gmail.com>
2025-08-02 00:17:52 -0300

    chore: Expose env to show dotenv config. Posgres general user restore.

--- part4/backend/config.py
+++ part4/backend/config.py
@@ -8,9 +8,9 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
     DEBUG = False
 
-    DB_USER = os.getenv("DB_USER", "usuario_por_defecto")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "contraseña")
-    DB_NAME = os.getenv("DB_NAME", "basededatos")
+    DB_USER = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+    DB_NAME = os.getenv("DB_NAME", "postgres")
     DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_PORT = os.getenv("DB_PORT", "5432")
 
@@ -18,7 +18,6 @@ class Config:
         f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 
 class DevelopmentConfig(Config):
     DEBUG = True
