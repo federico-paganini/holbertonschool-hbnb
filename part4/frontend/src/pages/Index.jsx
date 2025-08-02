a80334827deb37509ca34ea13776134fff506e17
federico-paganini <perfedefer@gmail.com>
2025-08-02 00:23:22 -0300

    feat: Places dashboard now added in Home, No welcome h1.

--- part4/frontend/src/pages/Index.jsx
+++ part4/frontend/src/pages/Index.jsx
@@ -1,9 +1,21 @@
+import PlacesDashboard from '../components/PlacesDashboard';
+
 const Home = () => {
   return (
     <>
       <title>HBnB | Home</title>
       <meta property="og:title" content="HBnB | Home" />
-      <h1>Welcome</h1>
+      <meta
+        name="description"
+        content="Welcome to HBnB, your home away from home."
+      />
+      <meta
+        property="og:description"
+        content="Welcome to HBnB, your home away from home."
+      />
+      <div className="container">
+        <PlacesDashboard />
+      </div>
     </>
   );
 };
